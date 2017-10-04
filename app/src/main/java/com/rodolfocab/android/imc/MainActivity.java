ff6d44c3d3d969c9142c8001e13271ab7c43a5c0
Rodolfo Emmanuel Cab Martinez <14470207@itcampeche.edu.mx>
2017-10-04 17:10:07 -0600

    IMC:Version inicial del proyecto. Solo muestra la interfaz de usuario y no incluye manejo de eventos.

---
+++ app/src/main/java/com/rodolfocab/android/imc/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rodolfocab.android.imc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
